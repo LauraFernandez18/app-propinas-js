853d3156fe9e562243333e864898bd90b7f96e54
LauraFernandez18 <10000068.joan23@fje.edu>
2021-10-11 20:04:12 +0200

    calculadora finalizada

--- processes/code.js
+++ processes/code.js
@@ -1,3 +1,19 @@
 function calculo() {
-    alert("hellow");
+    let totalCuenta = document.getElementById('total').value;
+    let servicio = document.getElementById('servicio').value;
+    let personas = document.getElementById('personas').value;
+    let resultado = (servicio * totalCuenta) / personas;
+    console.log('resultado: ' + resultado);
+    if (isNaN(resultado)) {
+        document.getElementById("res").innerHTML = "Introduce los valores"
+    } else if (personas == '' || personas == 0) {
+        document.getElementById("res").innerHTML = "Introduce datos válidos"
+    } else {
+        document.getElementById("res").innerHTML = "<p>total a pagar</p> <h3>" + resultado + " por persona";
+    }
+}
+
+function limpiar() {
+    document.getElementById("res").innerHTML = '';
+    document.getElementById("form").reset();
 }
